1ae1771ea8e052d08ba0f249c9c1bc0f3565a9bf
TheBoringDude <iamcoderx@gmail.com>
2021-07-19 15:33:40 +0800

    restructure

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import os
 import discord
-import logging
+from discord.ext import commands
+from discord.ext.commands import Context
+from logger import setup_logger
 
 
 # load .envfile if in development
 devel = os.getenv("DEVEL")
 if devel:
     from dotenv import load_dotenv
+
     load_dotenv(dotenv_path=".env.local")
 
-client = discord.Client()
+
+# get TOKEN from environment
 my_secret = os.getenv("TOKEN")
 
+# CREATE A NEW CLIENT
+intents = discord.Intents.default()
+intents.members = True
+client = commands.Bot(command_prefix=">", description="", intent=intents)
 
-logger = logging.getLogger("discord")
-logger.setLevel(logging.INFO)
-handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
-handler.setFormatter(
-    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
-)
-logger.addHandler(handler)
+# logger
+setup_logger()
 
 
 @client.event
@@ -27,15 +30,12 @@ async def on_ready():
     print("We have logged in as {0.user}".format(client))
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith("$hello"):
-        await message.channel.send(
-            "Hello! This bot is still under construction, please come back later"
-        )
+@client.command()
+async def hello(ctx: Context):
+    await ctx.channel.send(
+        "Hello! This bot is still under construction, please come back later"
+    )
 
 
+# run bot
 client.run(my_secret)
